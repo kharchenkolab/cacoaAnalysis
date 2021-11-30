2617e722edceb65bf85fec41167eef2d28aa0535
viktor_petukhov <viktor.s.petuhov@ya.ru>
2021-11-30 11:58:45 -0500

    Extended helpers

--- R/helpers.R
+++ R/helpers.R
@@ -1,3 +1,6 @@
+#' @import magrittr
+#' @import ggplot2
+
 createPagoda <- function(cm, min.transcripts.per.cell=0, dub.scores=NULL, dub.threshold=0.25) {
   cm <- cm[-grep("^MT-", rownames(cm)), ]
   if (!is.null(dub.scores)) {
@@ -17,7 +20,7 @@ createConos <- function(p2s, sample.meta, cell.meta, n.cores=1, k=20, ...) {
   con <- conos::Conos$new(p2s, n.cores=n.cores)
   con$buildGraph(k=k, k.self=5, space='PCA', ncomps=30, n.odgenes=2000, matching.method='mNN',
                  metric='angular', verbose=TRUE)
-  con$embedGraph(method='UMAP', min.prob.lower=1e-4, verbose=FALSE, ...);
+  con$embedGraph(method='UMAP', min.prob.lower=1e-6, verbose=FALSE, ...);
 
   con$misc$cell_metadata <- lapply(cell.meta, `[`, names(con$getDatasetPerCell()))
   con$misc$sample_metadata <- lapply(sample.meta, `[`, names(con$samples))
@@ -35,4 +38,89 @@ readOrCreate <- function(path, create.fun, force=FALSE) {
   res <- create.fun()
   readr::write_rds(res, path)
   return(res)
+}
+
+initializeCacoa <- function(dataset.name, ...){
+  dataset.name %<>% toupper()
+  con <- dataorganizer::DataPath(dataset.name, "con.rds") %>%
+    readr::read_rds() %>% conos::Conos$new()
+  ref.level <- 'Control'
+  cell.groups <- con$misc$cell_metadata$cellType # works for most datasets
+
+  if (dataset.name == 'AZ') {
+    target.level <- 'Alzheimer'
+    sample.groups <- con$misc$sample_metadata$sample %>%
+      {setNames(ifelse(startsWith(., 'A'), target.level, ref.level), .)}
+  } else if (dataset.name == 'ASD') {
+    target.level <- 'ASD'
+    sample.groups <- con$misc$sample_metadata$diagnosis
+  } else if (dataset.name == 'EP') {
+    target.level <- 'Epilepsy'
+    sample.groups <- con$misc$sample_metadata$Alias %>%
+      {setNames(ifelse(startsWith(., 'E'), target.level, ref.level), .)}
+    cell.groups <- con$misc$cell_metadata$l4
+  } else if (dataset.name == 'MS') {
+    target.level <- 'MS'
+    sample.groups <- con$misc$sample_metadata$diagnosis
+    cell.groups <- con$misc$cell_metadata$cell_type
+  } else if (dataset.name == 'PF') {
+    target.level <- 'IPF'
+    sample.groups <- con$misc$sample_metadata$Diagnosis
+  } else if (dataset.name == 'SCC') {
+    target.level <- 'Tumor'
+    sample.groups <- names(con$samples) %>%
+      {setNames(ifelse(grepl('Normal', .), ref.level, target.level), .)}
+    cell.groups <- con$misc$cell_metadata$level3_celltype
+  }
+
+  cao <- cacoa::Cacoa$new(
+    con, sample.groups=sample.groups, cell.groups=cell.groups,
+    target.level=target.level, ref.level=ref.level, ...
+  )
+
+  bs <- if (length(levels(cao$cell.groups)) > 20) 10 else 16
+  cao$plot.theme <- theme_bw(base_size = bs) +
+    theme(plot.title=element_text(hjust = 0.5),
+          legend.background=element_rect(fill=alpha("white", 0.2)),
+          legend.text=element_text(size=12),
+          legend.margin=margin(6, 6, 4, 1, 'pt'),
+          plot.margin=margin())
+
+  set.seed(239)
+  cao$cell.groups.palette <- levels(cao$cell.groups) %>%
+    {setNames(sample(brewerPalette("Paired")(length(.))), .)}
+  cao$sample.groups.palette <- c("#d73027", "#4575b4") %>%
+    setNames(c(cao$target.level, cao$ref.level))
+
+  return(cao)
+}
+
+runCacoaAnalyses <- function(cao, cluster.free.de=FALSE, cluster.based.de=FALSE, cell.density=c('graph', 'kde'),
+                             verbose=2) {
+  verb1 <- (verbose > 0)
+  verb2 <- (verbose > 1)
+  cao$estimateExpressionShiftMagnitudes(n.permutations=2500, verbose=verb1)
+  cao$estimateCellLoadings()
+
+  for (met in c('kde', 'graph')) {
+    if (!(met %in% cell.density))
+      next
+
+    cn <- paste0('cell.density.', met)
+    cao$estimateCellDensity(method=met, estimate.variation=FALSE, verbose=verb1, name=cn, beta=10)
+    cao$estimateDiffCellDensity(type='wilcox', adjust.pvalues=TRUE, verbose=verb1, n.permutations=500, name=cn)
+    cao$estimateDiffCellDensity(type='subtract', adjust.pvalues=FALSE, verbose=verb1, name=cn)
+  }
+
+  cao$estimateClusterFreeExpressionShifts(n.top.genes=3000, gene.selection="expression", verbose=verb2)
+
+  if (cluster.free.de) {
+    cao$estimateClusterFreeDE(n.top.genes=1500, min.expr.frac=0.01, adjust.pvalues=TRUE, smooth=TRUE)
+    cao$smoothClusterFreeZScores(progress.chunks=10, z.adj=TRUE)
+  }
+
+  if (cluster.based.de) {
+    cao$estimateDEPerCellType(independent.filtering=TRUE, test='DESeq2.Wald')
+    cao$estimateOntology(type="GSEA", org.db=org.Hs.eg.db::org.Hs.eg.db)
+  }
 }
